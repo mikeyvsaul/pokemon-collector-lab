3bbf2d3718b6ba8a3e56e01e6f472ad1ac917ef9
Michael Saul <michaelsaul@MacBook-Pro.local>
2020-09-20 16:12:47 -0700

    Add Feeding Model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner'),
+)
+
 class Pokemon(models.Model):
   name = models.CharField(max_length=100)
   element = models.CharField(max_length=100)
@@ -9,3 +16,19 @@ class Pokemon(models.Model):
 
   def __str__(self):
     return self.name
+
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'pokemon_id': self.id})
+
+class Feeding(models.Model):
+  date = models.DateField('feeding date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+    )
+
+  pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Pokemon
+from .forms import FeedingForm
 
 from django.http import HttpResponse
 
@@ -16,4 +18,18 @@ def pokemons_index(request):
 
 def pokemon_detail(request, pokemon_id):
   pokemon = Pokemon.objects.get(id=pokemon_id)
-  return render(request, 'pokemons/detail.html', { 'pokemon': pokemon })
+  feeding_form = FeedingForm()
+  return render(request, 'pokemons/detail.html', { 'pokemon': pokemon, 'feeding_form': feeding_form })
+
+class PokemonCreate(CreateView):
+  model = Pokemon
+  fields = '__all__'
+  success_url = '/pokemons/'
+
+class PokemonUpdate(UpdateView):
+  model = Pokemon
+  fields = ['element', 'description', 'age']
+
+class PokemonDelete(DeleteView):
+  model = Pokemon
+  success_url = '/pokemons/'
